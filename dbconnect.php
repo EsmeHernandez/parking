70cec7c471935463a771da531f92978d0ac0b100
EsmeHernandez <112506821+EsmeHernandez@users.noreply.github.com>
2023-10-17 01:09:22 -0600

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,8 +1,8 @@
 <?php
-$host="localhost";
-$user="root";
-$psw="root1234";
-$db="parking-inteligente";
+$host="app-efdb019d-4982-4e05-9332-74782736329e-do-user-14722467-0.c.db.ondigitalocean.com";
+$user="doadmin";
+$psw="AVNS_HexEgCP5pp-MwzTswaD";
+$db="parking_inteligente";
 
 $miConexion=mysqli_connect($host, $user,$psw,$db);
 if(mysqli_errno($miConexion)){
